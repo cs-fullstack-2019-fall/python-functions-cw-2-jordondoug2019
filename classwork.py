95795cd68331ce73520b8f9a099a0b528a6b380a
autumn-ragland <raglanda998@gmail.com>
2019-08-26 13:49:11 -0500

    Graded 08-26-2019 13:44:48

--- classwork.py
+++ classwork.py
@@ -1,6 +1,9 @@
+
+
 def calculator():
     numInput= int(input("enter a number "))
     numInput2=int(input("Enter a 2nd number "))
+    # !! : "Use the function to get two numbers from the user, then pass the two numbers to a function". your math actions should be in a different funciton 
     print(numInput+numInput2)
     print(numInput-numInput2)
     print(numInput*numInput2)
