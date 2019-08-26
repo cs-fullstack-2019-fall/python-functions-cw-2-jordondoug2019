9a9223635420ab8fb2f13f925e20481c38d29c55
jordondoug2019 <jdouglas2015@gmail.com>
2019-08-26 12:20:30 -0500

    graded cw

---
+++ classwork.py
@@ -0,0 +1,12 @@
+def calculator():
+    numInput= int(input("enter a number "))
+    numInput2=int(input("Enter a 2nd number "))
+    print(numInput+numInput2)
+    print(numInput-numInput2)
+    print(numInput*numInput2)
+    print(numInput/numInput2)
+
+def number():
+    calculator()
+
+number()
